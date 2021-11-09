6880d6f487f2307ab448202c9ce340ca5ecbeb86
limzkil <78449568+limzkil@users.noreply.github.com>
2021-11-09 16:23:40 -0500

    added conditional render button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import Peak from "./components/Peak.jsx";
+import  { useState } from "react";
 
 export default function App() {
+  
+  const [lookRegister, setLookRegister] = useState(false)
+  const [buttonText, setButtonText] = useState("Open the registry.")
+
+  const onClick = () => {
+    if (lookRegister === false) {
+      setLookRegister(true)
+      setButtonText("Close the registry.")
+    } else {
+      setLookRegister(false)
+      setButtonText("Open the registry.")
+      
+    }
+  }
   return (
     <>
-      <Peak />
+    <h1>Welcome to the top of Fin Dome!</h1>
+    <button onClick = {onClick}>{buttonText}</button>
+      <Peak lookRegister = {lookRegister} />
     </>
   );
 }
--- src/components/Peak.jsx
+++ src/components/Peak.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-export default function Peak() {
+export default function Peak(props) {
   const [allEntries, setAllEntries] = useState([]);
 
   useEffect(() => {
@@ -13,33 +13,42 @@ export default function Peak() {
       });
   }, [setAllEntries]);
 
-  return (
-    <>
-      <div>
-        {allEntries.map((entry) => {
-          return (
-            <div>
-              <h1>{entry.name}</h1>
-              <h1>{entry.date}</h1>
-              <h1>{entry.msg}</h1>
-              <form action = {`/scribble/${entry._id}`} method = "POST">
+
+  if (props.lookRegister === true) {
+
+    return (
+      <>
+        <div>
+          {allEntries.map((entry) => {
+       
+            return (
+              <div key = {entry._id}>
+                <h1>{entry.name}</h1>
+                <h1>{entry.date}</h1>
+                <h1>{entry.msg}</h1>
+                <form action={`/scribble/${entry._id}`} method="POST">
                   <button>Scribble this out?</button>
-              </form>
-              <form action = {`/pencil-in/${entry._id}`} method = "POST">
+                </form>
+                <form action={`/pencil-in/${entry._id}`} method="POST">
                   <button>Pencil something in?</button>
-              </form>
-              
-              
-            </div>
-          );
-        })}
-      </div>
-      <form action="/write" method="POST">
-        <input type="text" name="name" placeholder="Who are you?" />
-        <input type="text" name="date" placeholder="When were you here?" />
-        <input type="text" name="msg" placeholder="What do you have to say?" />
-        <input type="submit" value="Write in the log" />
-      </form>
-    </>
-  );
+                </form>
+              </div>
+            );
+          })}
+        </div>
+        <form action="/write" method="POST">
+          <input type="text" name="name" placeholder="Who are you?" />
+          <input type="text" name="date" placeholder="When were you here?" />
+          <input
+            type="text"
+            name="msg"
+            placeholder="What do you have to say?"
+          />
+          <input type="submit" value="Write in the log" />
+        </form>
+      </>
+    );
+  } else {
+      return (null);
+  }
 }
